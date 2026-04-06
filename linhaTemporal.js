5e0b8e97850d06bd2a18f373f0e996950bf48a93
AxlDog <axldogaxl@gmail.com>
2026-04-06 13:35:22 -0300

    Update linhaTemporal.js

--- linhaTemporal.js
+++ linhaTemporal.js
@@ -1,55 +1,18 @@
 
 const DATA_TIMELINE = {
-  "ato_1": {
-    "Sessao 1": {
-      "Localizacao inicial": "Baldur's Gate",
+  "ato_2": {
+    "Sessao Frieren": {
+      "Localizacao inicial": "Forte dos bandidos",
       "evento inicial": {
-        "descricao": "Wyll faz discurso e despacha os jogadores para Elturel na companhia de um guia.",
+        "descricao": "Frieren havia se entregado a bandidos para poder localizar e derrotar alguns demonios que estavam atrás dela.",
         "npc principal": {
-          "nome": "Wyll",
-          "titulo": "Lâmina das Fronteiras",
-          "discursos": [
-            "Aventureiros. Estranhos de terras distantes...",
-            "Seja qual for o caminho que os trouxe até aqui, ele os trouxe no momento certo..."
+          "nome": "Frieren",
+          "titulo": "Matadora de Demonios",
+          "objetivos": [
+            "Ajudar Frieren a derrotar os demonios",
           ]
         }
       },
-      "NPC em Perigo": "Ash e Tamir salvam uma Tiefling que estava fugindo de Elturel."
-    },
-    "sessao_2": {
-      "chegada_novos_personagens": [
-        {
-          "nome": "Helga",
-          "descricao": "Jogadora nova que chega trazendo Pimby.",
-          "npc_companheiro": {
-            "nome": "Pimby",
-            "raca": "Halfling",
-            "funcao": "Comercializa suprimentos com jogadores"
-          }
-        }
-      ],
-      "exploracao_locais_acampamento": [
-        {
-          "local": "Margem do Chiontar em um penhasco",
-          "evento": "Portal para Avernus é descoberto",
-          "acao_jogador": "Atira flecha no portal",
-          "consequencia": "Beholder surge e jogadores precisam fugir"
-        },
-        {
-          "local": "Último local indicado pelo guia",
-          "evento": "Drentches atacando jovem Tiefling",
-          "acao_jogador": "Jogadores matam os Drentches e salvam a jovem",
-          "npc_salvo": {
-            "nome": "Desconhecida",
-            "raca": "Tiefling"
-          }
-        }
-      ]
-    },
-    "sessao_3": {
-      "entrada_cidade": "Jogadores encontram Romero e Eskell no High Hall",
-      "conflito_templo": "Liberam templo tomado por cultistas de Asmodeus",
-      "descanso": "Taça Rubra"
+      "recompensas": "Frieren entrega a Kane e seu irmão um pergaminho do scrying para que possam localizar seu alvo ou possivelmente seu pai. Para Ashkarion ela da informações sobre sua espada e sobre Feanor."
     }
-  }
 };
