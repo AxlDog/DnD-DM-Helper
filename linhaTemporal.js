a5169ed9f850f4eead8d1d4c29c69e24a05d912f
AxlDog <axldogaxl@gmail.com>
2026-04-06 13:37:53 -0300

    Update linhaTemporal.js

--- linhaTemporal.js
+++ linhaTemporal.js
@@ -1,18 +1,19 @@
 
 const DATA_TIMELINE = {
-  "ato_2": {
-    "Sessao Frieren": {
-      "Localizacao inicial": "Forte dos bandidos",
-      "evento inicial": {
-        "descricao": "Frieren havia se entregado a bandidos para poder localizar e derrotar alguns demonios que estavam atrás dela.",
-        "npc principal": {
-          "nome": "Frieren",
-          "titulo": "Matadora de Demonios",
-          "objetivos": [
-            "Ajudar Frieren a derrotar os demonios",
-          ]
-        }
-      },
-      "recompensas": "Frieren entrega a Kane e seu irmão um pergaminho do scrying para que possam localizar seu alvo ou possivelmente seu pai. Para Ashkarion ela da informações sobre sua espada e sobre Feanor."
-    }
-};
+	"ato_2": {
+		"Sessao Frieren": {
+		  "Localizacao inicial": "Forte dos bandidos",
+		  "evento inicial": {
+			"descricao": "Frieren havia se entregado a bandidos para poder localizar e derrotar alguns demonios que estavam atrás dela.",
+			"npc principal": {
+			  "nome": "Frieren",
+			  "titulo": "Matadora de Demonios",
+			  "objetivos": [
+				"Ajudar Frieren a derrotar os demonios",
+			  ]
+			}
+		  },
+		  "recompensas": "Frieren entrega a Kane e seu irmão um pergaminho do scrying para que possam localizar seu alvo ou possivelmente seu pai. Para Ashkarion ela da informações sobre sua espada e sobre Feanor."
+		}
+	};
+}
